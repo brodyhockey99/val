eeceded558e47a267a9c1e4b8c7b24f335e56ecd
ODD <108240139+brodyhockey99@users.noreply.github.com>
2025-11-30 11:30:49 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,35 @@
-// login system placeholder
+let USER = null;
+
+function loadUser(username) {
+    let data = localStorage.getItem("FPS_USER_" + username);
+
+    if (!data) {
+        return {
+            username,
+            xp: 0,
+            level: 1,
+            operator: "Brim",
+            skinsOwned: [],
+            battlepassTier: 0
+        };
+    }
+    return JSON.parse(data);
+}
+
+function saveUser() {
+    if (!USER) return;
+    localStorage.setItem("FPS_USER_" + USER.username, JSON.stringify(USER));
+}
+
+function login() {
+    const name = usernameInput.value.trim();
+    if (name.length < 3) return alert("Too short.");
+
+    USER = loadUser(name);
+    saveUser();
+
+    loginMenu.style.display = "none";
+    mainMenu.style.display = "flex";
+}
+
+window.onload = () => loginMenu.style.display = "flex";
